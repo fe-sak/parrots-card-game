0ddf793c5dea8e3b3d92c28533d9203eb03de1a6
Felipe Sakuma <fycorsak@gmail.com>
2021-11-05 14:10:47 -0300

    Requisito: clique na carta

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let cardQuantity = prompt("Com quantas cartas deseja jogar? (Digite apenas números pares, de 4 a 14");
+let cardQuantity = 14; /* prompt("Com quantas cartas deseja jogar? (Digite apenas números pares, de 4 a 14"); */
 
 while (cardQuantity < 4 || cardQuantity > 14 || cardQuantity % 2 !== 0) {
     cardQuantity = prompt("Número digitado inválido. Por favor, digite novamente (apenas números pares, de 4 a 14");
@@ -17,30 +17,66 @@ function cardSpread() {
     let firstPair = [];
     let lastPair = [];
 
-    for (let i = 0; i < backFaces.length; i++) {
-        firstPair.push(backFaces[i]);
-    }
+    firstPair = [...backFaces];
 
     backFaces.sort(random);
 
-    for (let i = 0; i < backFaces.length; i++) {
-        lastPair.push(backFaces[i]);
-    }
+    lastPair = [...backFaces];
 
     for (let i = 0; i < backFaces.length; i++) {
-        document.querySelector(".cards-container").innerHTML = document.querySelector(".cards-container").innerHTML + `<div class="card" onclick="flipCard(this); pairCheck()">
-            <img class="front-face face" src="assets/front.png" alt="">
-            <img class="back-face face" src="assets/${firstPair[i]}.gif" alt="">
-        </div>`;
-        document.querySelector(".cards-container").innerHTML = document.querySelector(".cards-container").innerHTML + `<div class="card" onclick="flipCard(this) onclick="flipCard(this); pairCheck()">
-            <img class="front-face face" src="assets/front.png" alt="">
-            <img class="back-face face" src="assets/${lastPair[i]}.gif" alt="">
-        </div>`;
+        document.querySelector(".cards-container").innerHTML = document.querySelector(".cards-container").innerHTML +
+            `<div class="card" id="a${i}" onclick="flipCard(this); pairCheck(this)">
+                <div class="front-face face">
+                    <img  class="front-face" src="assets/front.png" alt="">
+                </div>
+                <div class="back-face face">
+                    <img  src="assets/${firstPair[i]}.gif" alt="">
+                </div>
+            </div>`;
+        document.querySelector(".cards-container").innerHTML = document.querySelector(".cards-container").innerHTML +
+            `<div class="card" id="b${i}" onclick="flipCard(this); pairCheck(this)">
+                <div class="front-face face">
+                    <img class="front-face" src="assets/front.png" alt="">
+                </div>
+                <div class="back-face face">
+                    <img src="assets/${lastPair[i]}.gif" alt="">
+                </div>
+             </div>`;
     }
 }
 
-function pairCheck() {
-    alert("A");
+async function pairCheck(clicked) {
+    let selected = document.querySelector(".selected");
+
+    if (selected !== null) { //segunda carta ->
+        if (selected.children[1].children[0].isEqualNode(clicked.children[1].children[0]) && selected.id !== clicked.id) {
+            selected.classList.add("paired");
+            clicked.classList.add("paired");
+
+            selected.classList.remove("selected");
+        }
+        else {
+            let cardsArray = document.querySelectorAll(".card");
+
+            for (let i = 0; i < cardsArray.length; i++) {
+                cardsArray[i].classList.add("pointer-events-none");
+            }
+
+            await sleep(1000);
+
+            selected.children[0].classList.remove("flipped");
+            selected.children[1].classList.remove("flipped");
+            selected.classList.toggle("selected");
+
+            clicked.children[0].classList.remove("flipped");
+            clicked.children[1].classList.remove("flipped");
+
+            for (let i = 0; i < cardsArray.length; i++) {
+                cardsArray[i].classList.remove("pointer-events-none");
+            }
+        }
+    }
+    else clicked.classList.toggle("selected");
 }
 
 function random() {
@@ -48,6 +84,15 @@ function random() {
 }
 
 function flipCard(clicked) {
-    clicked.children[0].classList.toggle("flipped");
-    clicked.children[1].classList.toggle("flipped");
+    if (!clicked.classList.contains("paired")) {
+        clicked.children[0].classList.toggle("flipped");
+        clicked.children[1].classList.toggle("flipped");
+    }
+}
+
+function timeOut() {
+    console.log("delay");
+}
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
